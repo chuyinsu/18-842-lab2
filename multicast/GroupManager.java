3e7cf2055933ebf86795ea0a3e6db6b331bed678
Hao Gao <hgao@twitter.com>
2014-02-08 17:34:20 -0500

    fix

--- multicast/GroupManager.java
+++ multicast/GroupManager.java
@@ -33,6 +33,7 @@ public class GroupManager {
 		this.seqVector = seqVector;
 		this.recvVector = Arrays.copyOf(seqVector, seqVector.length);
 		this.id = id;
+		this.members = members;
 	}
 
 	public String getName() {
